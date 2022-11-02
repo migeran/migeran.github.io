4283f28324e8ca8eec7abd282dc54076aa4146ad
Gergely Kis <gergely.kis@migeran.com>
2022-11-03 00:55:06 +0100

    Fix LinkedIn URL

---
+++ _next/static/chunks/pages/services-79176975c54f206e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[54],{9672:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/services",function(){return _(2003)}])},2003:function(n,u,_){"use strict";_.r(u);var e=_(5246);u.default=e.DM}},function(n){n.O(0,[445,275,371,509,246,774,888,179],(function(){return u=9672,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
